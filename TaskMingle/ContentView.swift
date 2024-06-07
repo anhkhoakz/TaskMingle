cd8a9d7b5f84eb15565824e86a0024c166a99124
anhkhoakz <113995598+anhkhoakz@users.noreply.github.com>
2024-06-07 18:52:24 +0700

    Initial Commit

---
+++ TaskMingle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskMingle
+//
+//  Created by Nguyễn Khoa on 7/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskMingle/TaskMingleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMingleApp.swift
+//  TaskMingle
+//
+//  Created by Nguyễn Khoa on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskMingleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
